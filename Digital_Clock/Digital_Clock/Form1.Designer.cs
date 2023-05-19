a00aa083d75fa7f29523d8c2a87043c51fd0a27c
1name4me <94053072+1name4me@users.noreply.github.com>
2023-05-19 20:33:32 +0100

    Add files via upload

---
+++ Digital_Clock/Digital_Clock/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Digital_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.Label();
+            this.DateOfNow = new System.Windows.Forms.Label();
+            this.clockTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(217, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Simple Digital Clock";
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 57.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time.Location = new System.Drawing.Point(228, 153);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(0, 87);
+            this.time.TabIndex = 1;
+            // 
+            // DateOfNow
+            // 
+            this.DateOfNow.AutoSize = true;
+            this.DateOfNow.Font = new System.Drawing.Font("Microsoft YaHei", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DateOfNow.Location = new System.Drawing.Point(236, 339);
+            this.DateOfNow.Name = "DateOfNow";
+            this.DateOfNow.Size = new System.Drawing.Size(345, 42);
+            this.DateOfNow.TabIndex = 2;
+            this.DateOfNow.Text = "24 Wednesday 2032";
+            // 
+            // clockTime
+            // 
+            this.clockTime.AutoSize = true;
+            this.clockTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clockTime.Location = new System.Drawing.Point(249, 167);
+            this.clockTime.Name = "clockTime";
+            this.clockTime.Size = new System.Drawing.Size(0, 73);
+            this.clockTime.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clockTime);
+            this.Controls.Add(this.DateOfNow);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Label DateOfNow;
+        private System.Windows.Forms.Label clockTime;
+    }
+}
+
---
+++ Digital_Clock/Digital_Clock/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Digital_Clock
+{
+    
+    public partial class Form1 : Form
+    {
+
+        System.Windows.Forms.Timer t = null;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartTimer();
+            date();
+        }
+
+        private void StartTimer()
+        {
+            t = new System.Windows.Forms.Timer();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(t_Tick);
+            t.Enabled = true;
+        }    
+
+        public void date()
+        {
+            DateOfNow.Text = DateTime.Today.ToString("dd dddd yyyy");
+        }
+        
+        private void t_Tick(object sender, EventArgs e)
+        {
+            time.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+       
+      
+    }
+}
